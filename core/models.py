882bad5904767e01065d1007714e1ca8f942bdf6
dinara <dineirashafigulllina@gmail.com>
2022-03-21 12:34:49 +0400

    тесты

--- core/models.py
+++ core/models.py
@@ -3,8 +3,7 @@ from django.db import models
 class Author(models.Model):
     name=models.CharField('Имя', max_length=128)
 
-    def __str__(self):
-        return self.name
+
 
 class Book(models.Model):
     author = models.ForeignKey('core.Author', on_delete=models.CASCADE, null=True, blank=True, related_name='books')
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from core import models
+
+
+class BookModel(TestCase):
+
+    def setUp(self):
+        self.book = models.Book.objects.create(name='Test Book')
+
+    def testStr(self):
+        self.assertEqual(
+            str(self.book),
+            'Test Book',
+            'Строковое представление объекта должно возвращать название'
+        )
+
+
+class BookSearchTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.book1 = models.Book.objects.create(name='Test Book 1')
+        self.book2 = models.Book.objects.create(name='Test Book 2')
+
+    def testWithoutParams(self):
+        response = self.client.get(reverse('core:book_list'))
+        self.assertEqual(200, response.status_code)
+        self.assertSequenceEqual(
+            list(response.context['object_list']),
+            list(models.Book.objects.all()),
+            'При поиске без параметров должны выводиться все книги',
+        )
+
+    def testSearchByName(self):
+        response = self.client.get(reverse('core:book_list'), data={'name': 'Test Book 1'})
+        self.assertEqual(1, response.context['object_list'].count())
+        self.assertEqual(
+            'Test Book 1',
+            response.context['object_list'].first().name,
+        )
